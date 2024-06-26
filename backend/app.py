570047780b606c75045d0316f9a9a4e6f12f9996
kartikeya-singh-422 <75291637+kartikeya-singh-422@users.noreply.github.com>
2024-06-26 10:45:46 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,32 @@
 # extract runtime data from the task manager of apps, create graphs and forward the data to frontend.
 
-# import numpy as np
-# import pandas as pd
+import numpy as np
+import pandas as pd
 from flask import Flask, jsonify
 from flask_cors import CORS
 import psutil
-from datetime import datetime
-import time
+from datetime import datetime, timedelta
 import os
+
+from device import get_device_info
 
 app = Flask(__name__)
 CORS(app)
+
 def get_process_info():
     process_list = []
     for process in psutil.process_iter(attrs=['pid', 'name', 'cpu_percent', 'memory_info', 'create_time']):
         try:
             process_info = process.info
+            create_time = datetime.fromtimestamp(process_info['create_time'])
+            process_info['create_time'] = create_time.strftime("%Y-%m-%d %H:%M:%S")
+            process_info['runtime'] = str(datetime.now() - create_time).split('.')[0]
+            process_info['memory_info'] = process_info['memory_info'].rss / (1024 * 1024)
             if process_info['cpu_percent'] > 0:
-                create_time = datetime.fromtimestamp(process_info['create_time'])
-                process_info['create_time'] = create_time.strftime("%Y-%m-%d %H:%M:%S")
+                process_info['end_time'] = "Currently running"
+            else:
                 process_info['end_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                process_info['runtime'] = str(datetime.now() - create_time).split('.')[0]
-                process_info['memory_info'] = process_info['memory_info'].rss / (1024 * 1024)
-                process_list.append(process_info)
+            process_list.append(process_info)
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     process_list = sorted(process_list, key=lambda p: p['cpu_percent'], reverse=True)
@@ -41,15 +45,15 @@ def processes():
     process_list = get_process_info()
     return jsonify(process_list)
 
+@app.route('/api/device_info', methods=['GET'])
+def device_info():
+    info = get_device_info()
+    return jsonify(info)
+
 if __name__ == "__main__":
      app.run(debug=True, port=5000)
-     """     while True:
-        process_list = get_process_info()
-        print_process_info(process_list)
-        time.sleep(15) """
 
-
-
-
-
-
+     # while True:
+     #   process_list = get_process_info()
+     #   print_process_info(process_list)
+     #   time.sleep(30)
